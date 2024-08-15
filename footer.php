41a812c4e98b4e3530930492e03a0098eadaeb7e
DBryja <dawbryja@gmail.com>
2024-08-15 11:56:49 +0200

    Page transition anim bugfix

--- footer.php
+++ footer.php
@@ -5,9 +5,13 @@
 
 <script>
     const transitionBox = document.querySelector('.transition-box');
+    let isTransitioning = false;
 
     document.querySelectorAll('a').forEach(link => {
         link.addEventListener('click', (event) => {
+            if(isTransitioning) return;
+            isTransitioning = true;
+
             event.preventDefault();
             gsap.fromTo(transitionBox, {
                 y: "100%",
@@ -21,13 +25,26 @@
             });
         });
     });
-
-    window.addEventListener('DOMContentLoaded', () => {
+    function runTransitionAnimation() {
         gsap.to(transitionBox, {
             y: "-100%",
             duration: 0.6,
             ease: 'power4.inOut',
+            onComplete: () => {
+                transitionBox.classList.add('ranAnim');
+            }
         });
+    }
+
+    window.addEventListener('DOMContentLoaded', () => {
+        runTransitionAnimation()
+    });
+    window.addEventListener('load', () => {
+       setTimeout(() => {
+            if(!transitionBox.classList.contains('ranAnim')){
+                runTransitionAnimation()
+            }
+         }, 1500);
     });
 </script>
 
